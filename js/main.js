d9762f909f91e27106fbfd4e31ec78843ba8ad23
unknown <rogerjverag@gmail.com>
2021-08-31 16:11:34 -0400

    Resposive hasta 491px, boton de menu, boton mas info en cards

--- js/main.js
+++ js/main.js
@@ -5,3 +5,31 @@ fondo.addEventListener("scroll",function(){
     window.scroll()
 
 })
+
+//Ocultar-Mostrar nav
+function btnNav() {
+    let navA = document.getElementsByClassName('padre');
+    for (let i=0; i<navA.length; i++) {
+        navA[i].classList.toggle('hide-a');
+    }
+    navA[0].classList.toggle('hide-a');
+}
+
+//Ocultar-Mostrar info cards
+
+function btnCard1() {
+    let cardInf = document.querySelector('#card1');
+    cardInf.classList.toggle('hide-info');
+}
+function btnCard2() {
+    let cardInf = document.querySelector('#card2');
+    cardInf.classList.toggle('hide-info');
+}
+function btnCard3() {
+    let cardInf = document.querySelector('#card3');
+    cardInf.classList.toggle('hide-info');
+}
+function btnCard4() {
+    let cardInf = document.querySelector('#card4');
+    cardInf.classList.toggle('hide-info');
+}
